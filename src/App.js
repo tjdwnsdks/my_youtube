9b4705b7d7d37851a701ef76b3b0ba8b7482f1d0
tjdwnsdks <tjdwnsdks@naver.com>
2025-01-09 12:48:57 +0900

    SCSS 세팅

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      App
     </div>
   )
 }
 
 export default App
+
+/* 다 하면 src 폴더에 pages 폴더 만들어서 페이지 컴포넌트들 만들기 */
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; 
 
+
+/* 추가 -  src > assets > scss 폴더 구성 끝나고 와서 하기 */
+import './assets/scss/style.scss';
+
+/* 이거 하고 github에 올리기
+ git add .
+ git status  <=  새로 올라가는 파일들 리스트 확인 가능
+
+ */
+
+
+// 이거 하고 App.js 가기
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

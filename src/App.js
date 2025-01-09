1a4930f8f4e3ddab42f9e1d646dd5bbbf597d691
tjdwnsdks <tjdwnsdks@naver.com>
2025-01-10 03:33:39 +0900

    페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,86 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+
+/* 할일: 총 12개의 페이지(아래 있는거)를 만들고 연결할거임 
+      근데 아래 import 부분처럼 일일이 하나씩 하게되면 
+      만약 페이지가 100개일때 각각의 페이지가 로드 될때마다 100개를 다 처리하고 페이지가 불러진다. 그럼 너무 느려진다.
+      그래서 코드 스플리팅 이라는걸 쓸거임
+      ("코드 스플리팅" 이란 해당 페이지가 필요할때까지 로딩하지 않는것임.)
+*/
+// import Home from './pages/Home'
+// import Today from './pages/Today'
+// import Developer from './pages/Developer'
+// import Webd from './pages/Webd'
+// import Website from './pages/Website'
+// import Gsap from './pages/Gsap'
+// import Port from './pages/Port'
+// import Youtube from './pages/Youtube'
+// import Channel from './pages/Channel'
+// import Video from './pages/Video'
+// import Search from './pages/Search'
+// import Not from './pages/Not'
+
+
+/* 코드 스플리팅 - lazy loading */
+const Home = lazy(() => import ('./pages/Home'))
+const Today = lazy(() => import ('./pages/Today'))
+const Developer = lazy(() => import ('./pages/Developer'))
+const Webd = lazy(() => import ('./pages/Webd'))
+const Website = lazy(() => import ('./pages/Website'))
+const Gsap = lazy(() => import ('./pages/Gsap'))
+const Port = lazy(() => import ('./pages/Port'))
+const Youtube = lazy(() => import ('./pages/Youtube'))
+const Channel = lazy(() => import ('./pages/Channel'))
+const Video = lazy(() => import ('./pages/Video'))
+const Search = lazy(() => import ('./pages/Search'))
+const Not = lazy(() => import ('./pages/Not'))
+
+
+
+const Loading = <div className={'bg-red-700'}>Loding...</div>
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Suspense fallback={Loading}>
+        <Routes> {/* BrowserRouter 내에서 사용되며, 다양한 경로에 대한 라우트 정보를 정의합니다 */}
+          <Route path='/' element={<Home />} /> {/*  경로가 '/'인 경우, Home 컴포넌트를 렌더링합니다 */}
+          <Route path='/today' element={<Today />} /> 
+          <Route path='/developer' element={<Developer />} /> 
+          <Route path='/webd' element={<Webd />} /> 
+          <Route path='/website' element={<Website />} /> 
+          <Route path='/gsap' element={<Gsap />} /> 
+          <Route path='/port' element={<Port />} /> 
+          <Route path='/youtube' element={<Youtube />} /> 
+          <Route path='/channel/:channelId' element={<Channel />} /> 
+          {/* (:은 pathVariable 이란 뜻. /channel/{숫자} 올건데 그건 channelId 라고 변수명을 정해준거임) */}
+          <Route path='/video/:videoId' element={<Video />} /> 
+          <Route path='/search/:searchId' element={<Search />} /> 
+          <Route path='*' element={<Not />} /> 
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+
+
+    // <BrowserRouter> {/* 리액트 라우터의 BrowserRouter 컴포넌트로 감싸여 있습니다. 이 컴포넌트는 브라우저의 주소 히스토리 API를 사용하여 라우팅을 관리합니다. */}
+    //     <Routes> {/* BrowserRouter 내에서 사용되며, 다양한 경로에 대한 라우트 정보를 정의합니다 */}
+    //         <Route path='/' element={<Home />} /> {/*  경로가 '/'인 경우, Home 컴포넌트를 렌더링합니다 */}
+    //         <Route path='/today' element={<Today />} /> 
+    //         <Route path='/developer' element={<Developer />} /> 
+    //         <Route path='/webd' element={<Webd />} /> 
+    //         <Route path='/website' element={<Website />} /> 
+    //         <Route path='/gsap' element={<Gsap />} /> 
+    //         <Route path='/port' element={<Port />} /> 
+    //         <Route path='/youtube' element={<Youtube />} /> 
+    //         <Route path='/channel/:channelId' element={<Channel />} /> 
+    //         <Route path='/video/:videoId' element={<Video />} /> 
+    //         <Route path='/search/:searchId' element={<Search />} /> 
+    //         <Route path='*' element={<Not />} /> 
+    //     </Routes>
+    // </BrowserRouter>
+/*  src 폴더에 pages 폴더 만들어서 페이지 컴포넌트들 만들기 */
+
+);
 }
 
 export default App
-
-/* 다 하면 src 폴더에 pages 폴더 만들어서 페이지 컴포넌트들 만들기 */
--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ import App from './App';
 import './assets/scss/style.scss';
 
 /* 이거 하고 github에 올리기
- git add .
- git status  <=  새로 올라가는 파일들 리스트 확인 가능
+      git add .
+      git status  <=  새로 올라가는 파일들 리스트 확인 가능
+      git commit -m "SCSS 세팅"
+      git push -u origin main
+  이거 하고 깃허브 홈페이지 가서 확인해보면 됨
+  하고 새 브런치 '04-SCSS 세팅' 만들기
 
+  (해보면 이제부터 각 브런치 마다 버전별로 코드 볼 수 있다.)
  */
 
 
-// 이거 하고 App.js 가기
+
+  /* 다 하면 App.js 가서 BrowserRouter 걸기  */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
